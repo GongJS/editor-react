6e511977ce8103c34904fa6bf7056b3ed62bdfd2
gongjs <gongjiansheng@agora.io>
2021-11-30 11:37:08 +0800

    feat: 新增 recoil -> editor 数据

---
+++ src/store/editor.ts
@@ -0,0 +1,37 @@
+import {
+  atom,
+} from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
+
+interface ComponentData {
+  // 这个元素的 属性，属性请详见下面
+  props: { [key: string]: any };
+  // id，uuid v4 生成
+  id: string;
+  // 业务组件库名称 l-text，l-image 等等
+  name: string;
+}
+
+export interface EditorProps {
+  // 供中间编辑器渲染的数组
+  components: ComponentData[];
+  // 当前编辑的是哪个元素，uuid
+  currentElement: string;
+  // 当然最后保存的时候还有有一些项目信息，这里并没有写出，等做到的时候再补充
+}
+
+export const testComponents: ComponentData[] = [
+  { id: uuidv4(), name: 'l-text', props: { text: 'hello' } },
+  { id: uuidv4(), name: 'l-text', props: { text: 'hello2' } },
+  { id: uuidv4(), name: 'l-text', props: { text: 'hello3' } },
+];
+
+const editorData = atom({
+  key: 'editorData',
+  default: {
+    components: testComponents,
+    currentElement: '',
+  },
+});
+
+export default editorData;
--- src/views/editor/index.tsx
+++ src/views/editor/index.tsx
@@ -1,39 +1,48 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { useRecoilValue } from 'recoil';
+import editorData from '@/store/editor';
 import './style.less';
 
 const {
   Header, Content, Sider,
 } = Layout;
 
-const Editor: React.FC = () => (
-  <div className="editor" id="editor-layout-main">
-    <Layout style={{ background: '#fff' }}>
-      <Header className="header">
-        <div className="page-title" style={{ color: '#fff' }}>
-          慕课乐高
-        </div>
-      </Header>
-    </Layout>
-    <Layout>
-      <Sider width="300" style={{ background: 'yellow' }}>
-        <div className="sidebar-container">
-          组件列表
-        </div>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Content className="preview-container">
-          <p>画布区域</p>
-          <div className="preview-list" id="canvas-area">
-            123
+const Editor: React.FC = () => {
+  const { components } = useRecoilValue(editorData);
+  return (
+    <div className="editor" id="editor-layout-main">
+      <Layout style={{ background: '#fff' }}>
+        <Header className="header">
+          <div className="page-title" style={{ color: '#fff' }}>
+            乐高
           </div>
-        </Content>
+        </Header>
       </Layout>
-      <Sider width="300" style={{ background: 'purple' }} className="settings-panel">
-        组件属性
-      </Sider>
-    </Layout>
-  </div>
-);
+      <Layout>
+        <Sider width="300" style={{ background: 'yellow' }}>
+          <div className="sidebar-container">
+            组件列表
+          </div>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content className="preview-container">
+            <p>画布区域</p>
+            <div className="preview-list" id="canvas-area">
+              {
+              components.map((component) => (
+                component.props.text
+              ))
+            }
+            </div>
+          </Content>
+        </Layout>
+        <Sider width="300" style={{ background: 'purple' }} className="settings-panel">
+          组件属性
+        </Sider>
+      </Layout>
+    </div>
+  );
+};
 
 export default Editor;
