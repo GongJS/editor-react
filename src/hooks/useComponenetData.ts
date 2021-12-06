6f9eba49c5a3f42ad1e706bfa9d3b8e58a66d7c6
gongjs <gongjiansheng@agora.io>
2021-12-06 20:18:04 +0800

    feat: 新增 useComponentData hooks

---
+++ src/hooks/useComponenetData.ts
@@ -0,0 +1,37 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { AllComponentProps } from '@/defaultProps';
+import editorData, { ComponentData, getCurrentElement } from '@/store/editor';
+
+const useComponentData = () => {
+  const [editor, setEditor] = useRecoilState(editorData);
+  const copyComponents = JSON.parse(JSON.stringify(editor.components));
+  let currentElement = useRecoilValue(getCurrentElement);
+  const updateComponent = (key: keyof AllComponentProps, value: string) => {
+    if (!currentElement) return;
+    currentElement = JSON.parse(JSON.stringify(currentElement));
+    copyComponents.map((component: ComponentData) => {
+      if (component.id === currentElement?.id) {
+        component.props[key] = value;
+      }
+      return component;
+    });
+    setEditor((oldEditor) => ({
+      ...oldEditor,
+      components: copyComponents,
+    }));
+  };
+  const addComponent = (component: ComponentData) => {
+    if (!component) return;
+    const newComponents = [...editor.components, component];
+    setEditor((oldEditor) => ({
+      ...oldEditor,
+      components: newComponents,
+    }));
+  };
+  return {
+    updateComponent,
+    addComponent,
+  };
+};
+
+export default useComponentData;
