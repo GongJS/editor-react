f535afe673792cf1ba251603074efc1ee55128c4
gongjs <gongjiansheng@agora.io>
2021-12-24 12:05:43 +0800

    feat: deploy gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import {
-  RecoilRoot,
-} from 'recoil';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
+      <BrowserRouter basename="/editor-react">
         <App />
       </BrowserRouter>
     </RecoilRoot>
