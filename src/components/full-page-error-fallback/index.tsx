02bc48546601ef5bae6e5e8f18fcab21d985c4e4
gongjs <382663074@qq.com>
2021-12-31 23:23:05 +0800

    feat: 添加错误边界处理

---
+++ src/components/full-page-error-fallback/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Typography, Button } from 'antd';
+import './style.less';
+
+const isError = (value: any): value is Error => value?.message;
+
+export const ErrorBox = ({ error }: { error: unknown }) => {
+  if (isError(error)) {
+    return <Typography.Text type="danger">{error?.message}</Typography.Text>;
+  }
+  return null;
+};
+
+const FullPageErrorFallback: React.FC = ({ error, resetErrorBoundary }) => (
+  <div className="full-page-error-fallback">
+    <ErrorBox error={error} />
+    <Button style={{ marginTop: '50px' }} type="primary" onClick={resetErrorBoundary}>
+      Reset
+    </Button>
+  </div>
+);
+
+export default FullPageErrorFallback;
--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { ErrorBoundary } from 'react-error-boundary';
 import FullPageLoading from '@/components/full-page-loading';
+import FullPageErrorFallback from '@/components/full-page-error-fallback';
 import App from './App';
 import './index.css';
 
+const errorHandler = (error: Error, info: { componentStack: string }) => {
+  // Todo
+  console.log('error:', error, 'info:', info);
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <React.Suspense fallback={<FullPageLoading />}>
-      <RecoilRoot>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </RecoilRoot>
-    </React.Suspense>
+    <ErrorBoundary FallbackComponent={FullPageErrorFallback} onError={errorHandler}>
+      <React.Suspense fallback={<FullPageLoading />}>
+        <RecoilRoot>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </RecoilRoot>
+      </React.Suspense>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root'),
 );
